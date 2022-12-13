0f9992594d55a160cd0fcc39972c5cac886735ca
Cam <cam.ball96@hotmail.com>
2022-12-14 00:19:13 +1000

    Added new container components to become a nested component for other components. Added some more CSS to project page and mapped out data to project cards. Will be updating project cards with 3 separate projects next

--- src/components/BodyContact/BodyContact.jsx
+++ src/components/BodyContact/BodyContact.jsx
@@ -1,36 +1,32 @@
-import React from 'react'
-import BioInfo from '../Bio/BioInfo'
-import Form from '../Form/Form'
-import './bodyContact.css'
-import ContactBG from "../../assets/contactBg.jpg"
-import { aboutBody } from "../../utils/Utils"
-
+import React from "react";
+import BioInfo from "../Bio/BioInfo";
+import Form from "../Form/Form";
+import "./bodyContact.css";
+import ContactBG from "../../assets/contactBg.jpg";
+import { aboutBody } from "../../utils/Utils";
+import Container from "../Container/Container";
 
 function BodyContact() {
-
-    function createForm(x) {
-        return(
-            <label key={x.id} label={x.label} />
-        )
-    }
-
-
-
-    return (
-        <div className='bodyContact'>
-            <div className='imageLeft'>
-                <img className='bcImage' src={ContactBG}></img>
-            </div>
-            <div className="infoRight">
-                <div className='bioInfo'>
-                <BioInfo aboutHeading= "Want to reach out?" aboutBody= {aboutBody} cname = "cHeading" bname="subHeading"/>
-                </div>
-                <div className='form'>
-                <Form />
-                </div>
-            </div>
+  return (
+    <div className="bodyContact">
+      <div className="imageLeft">
+        <img className="bcImage" src={ContactBG}></img>
+      </div>
+      <Container className="infoRight">
+        <div className="bioInfo">
+          <BioInfo
+            aboutHeading="Want to reach out?"
+            aboutBody={aboutBody}
+            cname="cHeading"
+            bname="subHeading"
+          />
         </div>
-    )
+        <div className="form">
+          <Form />
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default BodyContact;
